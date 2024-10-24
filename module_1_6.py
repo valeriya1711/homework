86e6c1b6cb36e1bea53d3e2b0d21f28b468dbbda
valeriya1711 <valeriya.rezerv18@gmail.com>
2024-10-24 22:36:47 +0500

    переменные

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+completed_homework = 12
+print(completed_homework, type(completed_homework))
+hours_spent = 1.5
+print(hours_spent, type(hours_spent))
+course_name = 'python'
+print(course_name, type(course_name))
+time_per_task = hours_spent/completed_homework
+print(time_per_task, type(time_per_task))
+print(course_name, completed_homework, hours_spent, time_per_task)
